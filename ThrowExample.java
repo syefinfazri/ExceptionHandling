7f267433be78c3b64b61cee54d1a6f6e4ce0d787
syefinfazri <149354982+syefinfazri@users.noreply.github.com>
2025-06-14 10:31:41 +0700

    Add files via upload

---
+++ ThrowExample.java
@@ -0,0 +1,16 @@
+public class ThrowExample {
+    static void demo() {
+        NullPointerException t;
+        t = new NullPointerException("Coba Throw");
+        throw t;
+    }
+
+    public static void main(String[] args) {
+        try {
+            demo();
+            System.out.println("Selesai");
+        } catch (NullPointerException e) {
+            System.out.println("Ada pesan error: " + e);
+        }
+    }
+}
